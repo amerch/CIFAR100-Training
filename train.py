53192cd9156b0e65e0e575c770f4efe81e4e9f21
amerch <amilmerchant@college.harvard.edu>
2018-07-15 19:27:42 -0700

    Normalize for cifar100

--- train.py
+++ train.py
@@ -59,14 +59,14 @@ if args.augment:
         transforms.RandomCrop(32, padding=4),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465),
-                             (0.2023, 0.1994, 0.2010)),
+        transforms.Normalize((0.5071, 0.4867, 0.4408),
+                             (0.2675, 0.2565, 0.2761)),
     ])
 else:
     transform_train = transforms.Compose([
         transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465),
-                             (0.2023, 0.1994, 0.2010)),
+        transforms.Normalize((0.5071, 0.4867, 0.4408),
+                             (0.2675, 0.2565, 0.2761)),
     ])
 
 
